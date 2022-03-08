2b0089d5b9089c213cf57b4fa34baa432d12416a
Abhishek Kumar <abhishek.kum1908@gmail.com>
2022-03-08 17:33:15 +0530

    Main page

--- components/Body.js
+++ components/Body.js
@@ -4,7 +4,14 @@ import styles from "./Body.module.css";
 function Body() {
   return (
     <div className="">
-      <div className={styles.gradientText}>bringing designs to life</div>
+      <div className={`${styles.gradientText} text-center`}>
+        Bringing designs to life
+      </div>
+      <div>Code</div>
+      <div>
+        Hello, I'm Abhishek. Welcome to my digital garden where I share my
+        learnings and the stuff which I've made.{" "}
+      </div>
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Header from "../components/Header";
 import Body from "../components/Body";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       </Head>
       <Header />
       <Body />
+      <Footer />
     </div>
   );
 }
